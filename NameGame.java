1c3616921d6141274580ca426249ac40e31f3a0b
lajacl <lajacl@yahoo.com>
2016-09-26 04:15:33 -0400

    Add files via upload

--- NameGame.java
+++ NameGame.java
@@ -14,25 +14,25 @@ public class NameGame
     String name= ""; // user input name
     String nameSub = ""; // name starting with first vowel or y
     char letter; // used to find the first vowel in name
-    char initial; // used to prevent first initial letters clases with song
-    int nameStart; // used to show where where anem should begin based on first vowel location
+    char initial; // used to prevent first initial letters classes with song
+    int nameStart; // used to show where where a name should begin based on first vowel location
     int i; // iterator for loop creating sub name
 
     
         // Loops the program until the user is done making lyrics
-        while(!name.equals("DONE"))
+        while(!name.toUpperCase().equals("DONE"))
         {
           //Get user name input 
           name = JOptionPane.showInputDialog("Please enter a name to use for \"The Name Game\" song." +
-          "\n If you are done making lyrics enter DONE in all caps.");
+          "\n If you are done making lyrics enter DONE.");
           
-          // Prevent use of vulgar words in song by blcoking some names
+          // Prevent use of vulgar words in song by blocking some names
           if (name.toLowerCase().equals("mitch") || name.toLowerCase().equals("tucker") || name.toLowerCase().equals("buck"))
           {
             JOptionPane.showMessageDialog(null, "Please pick another name");
           }
           
-          else if (!name.equals("DONE"))
+          else if (!name.toUpperCase().equals("DONE"))
           {    
             // Create sub name that begin with a vowel 
             letter = name.charAt(1);
@@ -62,24 +62,24 @@ public class NameGame
                 case 'b':
                 case 'B':
                     JOptionPane.showMessageDialog(null, "~The Name Game~\n\n" + name + "!\n" + 
-                    name + ", " + name + " bo " + nameSub + " Banana fanna fo F" + nameSub + 
+                    name + ", " + name + " bo " + nameSub + " \nBanana fanna fo F" + nameSub + 
                     "\nFee fy mo M" + nameSub + ", " + name + "!");                     
                   break;
                 case 'f':
                 case 'F':
                     JOptionPane.showMessageDialog(null, "~The Name Game~\n\n" + name + "!\n" + 
-                    name + ", " + name + " bo B" + nameSub + " Banana fanna fo " + nameSub + 
+                    name + ", " + name + " bo B" + nameSub + " \nBanana fanna fo " + nameSub + 
                     "\nFee fy mo M" + nameSub + ", " + name + "!");                    
                   break; 
                 case 'm':
                 case 'M':
                     JOptionPane.showMessageDialog(null, "~The Name Game~\n\n" + name + "!\n" + 
-                    name + ", " + name + " bo B" + nameSub + " Banana fanna fo F" + nameSub + 
+                    name + ", " + name + " bo B" + nameSub + " \nBanana fanna fo F" + nameSub + 
                     "\nFee fy mo " + nameSub + ", " + name + "!");                     
                   break;
                 default:
                     JOptionPane.showMessageDialog(null, "~The Name Game~\n\n" + name + "!\n" + 
-                    name + ", " + name + " bo B" + nameSub + " Banana fanna fo F" + nameSub + 
+                    name + ", " + name + " bo B" + nameSub + " \nBanana fanna fo F" + nameSub + 
                     "\nFee fy mo M" + nameSub + ", " + name + "!");                        
              }  
              
